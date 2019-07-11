2cf0141a6b469559868242201bb45361ffe62420
Antonio Devís <colomett@gmail.com>
2019-07-11 13:37:37 +0200

    new anotations

--- extensions-for-pressbooks.php
+++ extensions-for-pressbooks.php
@@ -1,17 +1,32 @@
 <?php
-/*
 
-Plugin Name:  Extensions for PressBooks
-Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
-Description:  Small enhancement for Pressbooks main plugin
-Version:      1.2.3
-Author:       @huguespages (My Language Skills)
-Author URI:   https://developer.wordpress.org/
-License:      GPL 3.0
-License URI:  https://www.gnu.org/licenses/gpl-3.0.html
-Text Domain:  extensions-for-pressbooks
-Domain Path:  /languages
-*/
+/**
+ * Extensions for PressBooks
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/my-language-skills/extensions-for-pressbooks
+ * @since             1.0
+ * @package           extensions-for-pressbooks
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Extensions for PressBooks
+ * Plugin URI:        https://github.com/my-language-skills/extensions-for-pressbooks
+ * Description:       Small enhancement for Pressbooks main plugin
+ * Version:           1.2.3
+ * Author:            My Language Skills team
+ * Author URI:        https://github.com/my-language-skills/
+ * License:           GPL 3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       extensions-for-pressbooks
+ * Domain Path:       /languages
+ */
+
+
+
 
 defined ("ABSPATH") or die ("No script assholes!");
 
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== All in one metadata ===
+Contributors: colomet, danzhik, huguespages
+Donate link: https://opencollective.com/mylanguageskills
+Tags: schema, schema.org, metadata, rich snippets, structured data
+Requires at least: 3.0.1
+Tested up to: 5.2.2
+Requires PHP: 5.6
+Stable tag: 1.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Extended Metadata for Wordpress and PressBooks.
+
+== Description ==
+
+soon
+== Installation ==
+
+1. Clone (or copy) this repository folder `extensions-for-pressbooks` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' screen in WordPress
+
+== Frequently Asked Questions ==
+
+= question =
+
+answer
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+ First release
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+First release
+
+== Disclaimers ==
+
+The Extensions for PressBooks plugin is supplied "as is" and all use is at your own risk.
+
+Extensions for PressBooks is under development. Version 1.0 is stable but is an start. Soon much more possibilities.
