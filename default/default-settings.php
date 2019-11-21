f779bb84d28e1e5b18949236e970f4b2750ebcbd
Antonio Devís <colomett@gmail.com>
2019-11-21 15:07:03 +0100

    version

--- default/default-settings.php
+++ default/default-settings.php
@@ -9,13 +9,13 @@
  *
  * @package extensions-for-pressbooks
  * @subpackage Functionality/Defatult options
- * @since 1.2.4 (when the file was introduced)
+ * @since 1.2.5 (when the file was introduced)
  */
 
 /**
  * Force new websites to use books4languages-child-theme-for-pressbooks
  *
- * @since 1.2.4
+ * @since 1.2.5
  *
  */
 
