2bad9608fe320ff3b4b132b9db862db4343cf86a
AraldGarbo <42145494+AraldGarbo@users.noreply.github.com>
2020-01-28 15:38:25 +0100

    update canonical
    solved issue when book is not featured

--- canonical/efpb-canonical.php
+++ canonical/efpb-canonical.php
@@ -12,20 +12,10 @@
  * @package           extensions-for-pressbooks
  *
  **/
-$optionnes = get_blog_option(null,'efpb_canonical_metabox_enable', false );
-echo "....................................................................................";
-var_dump($optionnes);
-echo "....................................................................................";
-echo $optionnes;
 
  if (is_plugin_active('autodescription/autodescription.php')){
-   // global $wpdb;
-   // $blog_id =get_current_blog_id();
-   // $wp_blog_id_options = "wp_".$blog_id."_options";
-   // $query_canonical_metabox = "SELECT option_value FROM `$wp_blog_id_options` WHERE option_name = 'efpb_canonical_metabox_enable'";
-   // $result_canonical_metabox = $wpdb->get_var($query_canonical_metabox);
    $result_canonical_metabox = get_blog_option(null,'efpb_canonical_metabox_enable');
-   if ( $result_canonical_metabox == 1 ){
+   if ( $result_canonical_metabox == 1  && book_is_a_featured2()){
      add_filter( 'the_seo_framework_rel_canonical_output', 'get_canonical_url' );
    }
 
@@ -35,72 +25,18 @@ echo $optionnes;
   *
   **/
    function get_canonical_url(){
-     // global $wpdb;
-     // $blog_id =get_current_blog_id();
-     // $wp_blog_id_options = "wp_".$blog_id."_options";
-     // $query_is_original = "SELECT option_value FROM `$wp_blog_id_options` WHERE option_name = '_transient_pb_book_source_url'";
-     // $result_is_original = $wpdb->get_var($query_is_original);
      $result_is_original = get_blog_option(null, '_transient_pb_book_source_url');
      return $result_is_original;
    }
  }
 
-// /**
-//  * :
-// **/
-// //It works only with "The SEO Framework" plugin active
-//   add_action('wp_ajax_efpb_mark_canonical', 'efpb_mark_canonical', 1);
-//   add_action('admin_enqueue_scripts', 'efpb_om_enqueue_scripts_canonical');
-//
-//   //book is not featured or is featured and canonical checkbox = ON
-//   add_filter( 'the_seo_framework_rel_canonical_output', 'get_canonical_url' );
-//
-//
-// /**
-//  * FUNCTIONS:
-// **/
-//
-//  /**
-//   * Mark original link / father's link
-//   * @since 1.2.8
-//  **/
-// function efpb_mark_canonical() {
-//   if ( ! current_user_can( 'manage_network' ) || ! check_ajax_referer( 'pressbooks-aldine-admin' ) ) {
-//     return;
-//   }
-//   $blog_id = intval($_POST['book_id']);
-//   if(!$blog_id){
-//     return;
-//   }
-//   $blog_id = sanitize_text_field ($blog_id);
-//   $canonical = $_POST['canonical'];
-//   $canonical = sanitize_text_field ($canonical);
-//   if ( $canonical === 'true' ) {
-//     delete_blog_option( $blog_id, 'efp_publisher_canonical' );
-//     update_blog_option( $blog_id, 'efp_publisher_canonical', 1 );
-//   } else {
-//     delete_blog_option( $blog_id, 'efp_publisher_canonical' );
-//     update_blog_option( $blog_id, 'efp_publisher_canonical', 0 );
-//   }
-// }
-//
-// /**
-//  * Enqueue script
-//  * @since 1.2.8
-// **/
-// function efpb_om_enqueue_scripts_canonical () {
-//   wp_enqueue_script( 'canonical-script', plugin_dir_url( __FILE__ ).'assets/scripts/canonical-admin.js');
-// }
-//
-// /**
-//  * Return father's canonical URL
-//  * @since 1.2.8
-// **/
-// function get_canonical_url($result_is_original){
-//   global $wpdb;
-//   $blog_id =get_current_blog_id();
-//   $wp_blog_id_options = "wp_".$blog_id."_options";
-//   $query_is_original = "SELECT option_value FROM `$wp_blog_id_options` WHERE option_name = '_transient_pb_book_source_url'";
-//   $result_is_original = $wpdb->get_var($query_is_original);
-//   return $result_is_original;
-// }
+ function book_is_a_featured2 (){
+   if(get_current_blog_id() != 1){
+     $result_is_original = get_blog_option(null,'efp_publisher_is_original');
+     if ( $result_is_original == 1 ){
+       //book is featured
+       return true;
+     }
+   }
+   return false;
+ }
