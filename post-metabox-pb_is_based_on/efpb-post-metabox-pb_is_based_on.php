e2b823fc9965b1ccd49b49c6b9b69d1adb30877e
CharalamposTheodorou <31764602+CharalamposTheodorou@users.noreply.github.com>
2020-03-04 17:04:56 +0100

    Changes in post-metabox-pb_is_based_on directory
    - Translations functions on Titles
    - sanitize_option() on get_option()
    - sanitize_meta() on get_post_meta()
    - replace echo with _e() functions

--- post-metabox-pb_is_based_on/efpb-post-metabox-pb_is_based_on.php
+++ post-metabox-pb_is_based_on/efpb-post-metabox-pb_is_based_on.php
@@ -21,13 +21,15 @@ add_action( 'save_post', 'efp_pbibo_url_field_save', 10, 2 );
  *
  */
 function efp_init_pbibo_metabox(){
+  
   global $post;
-  $pbibo_enabled = get_option('efp_pbibo_metabox_enable' );
+  //$pbibo_enabled = get_option('efp_pbibo_metabox_enable' );
+  $pbibo_enabled = sanitize_option('efp_pbibo_metabox_enable',get_option('efp_pbibo_metabox_enable' ));
 
   // initialize the metabox only IF current post 'pb_is_based_on' meta_key exists AND 'efp_pbibo_metabox_enable' is set to 1
   if ( 1 == $pbibo_enabled){
         $post_types = ['metadata','front-matter','chapter','part', 'back-matter'];
-        add_meta_box( 'efp_pbibo_metabox', 'Change pb_is_based_on value', 'efp_render_pbibo_metabox', $post_types, 'side', 'low');
+        add_meta_box( 'efp_pbibo_metabox', __('Change pb_is_based_on value','extensions-for-pressbooks'), 'efp_render_pbibo_metabox', $post_types, 'side', 'low');
     }
   }
 
@@ -38,45 +40,51 @@ function efp_init_pbibo_metabox(){
  *
  */
 function efp_render_pbibo_metabox(){
-    global $post;
-    if (empty(get_post_meta($post->ID, 'pb_is_based_on', true))) { // If pb_is_based_on URL is not set print diferent output
-      $html = '<b>Current URL:</b>';
-      if (true != efp_is_site_clone()){
-         $html .= '<p style="word-wrap:break-word;">Site is considered as source.</p><hr>' ;
-      } else {
-         $html .= '<p style="word-wrap:break-word;">not set</p><hr>' ;
-      }
-      $html .= '<b>Set new URL:</b>';
-      $html .= '<input name="pb_is_based_on" id="efp_pbibo_metabox" type="url"  placeholder="http://example.com" size="25" />';
-      echo $html;
-      return;
-    }
-
-    $pbibo_url = get_post_meta($post->ID, 'pb_is_based_on');
-    $pbibo_url = reset($pbibo_url);
-
-    $pbibo_url_parse = parse_url($pbibo_url);                   // parse url
-    $pbibo_url_parse_host = $pbibo_url_parse["host"];           // get 'host' of the url
-    $pbibo_url_parse_path = $pbibo_url_parse["path"];           // get 'path' of the url
-
-    $site_url = get_site_url();                                 // get current site URL
-    $site_url_parse = parse_url($site_url);                     // parse this URL
-    $site_url_parse_host = $site_url_parse["host"];             // get 'host' of the url
-
-    // compare URLs of pb_is_based_on meta_key AND site URL
-    if ($site_url_parse_host == $pbibo_url_parse_host){
-      $url_to_print = $pbibo_url_parse_path;                    // If they are the same, print only URL path
+  global $post;
+ // if (empty(get_post_meta($post->ID, 'pb_is_based_on', true))) 
+  if (empty(sanitize_meta('pb_is_based_on',get_post_meta($post->ID,'pb_is_based_on',true),'url')))
+  { // If pb_is_based_on URL is not set print diferent output
+    $html = '<b>Current URL:</b>';
+    if (true != efp_is_site_clone()){
+       $html .= '<p style="word-wrap:break-word;">Site is considered as source.</p><hr>' ;
     } else {
-      $url_to_print = $pbibo_url;                               // If not, print full URL
+       $html .= '<p style="word-wrap:break-word;">not set</p><hr>' ;
     }
-
-    // generate HTML do be printed inside the metabox
-    $html = '';
-    $html .= '<b>Current URL:</b>';
-    $html .= '<p style="word-wrap:break-word;">'. $url_to_print .'</p><hr>' ;
-    $html .= '<b>Insert new URL:</b>';
+    $html .= '<b>Set new URL:</b>';
     $html .= '<input name="pb_is_based_on" id="efp_pbibo_metabox" type="url"  placeholder="http://example.com" size="25" />';
-    echo $html;
+    //echo $html;
+    _e($html);
+    return;
+  }
+  //$pbibo_url = get_post_meta($post->ID, 'pb_is_based_on');
+  $pbibo_url = sanitize_meta('pb_is_based_on',get_post_meta($post->ID,'pb_is_based_on'),'url');
+  $pbibo_url = reset($pbibo_url);
+
+  $pbibo_url_parse = parse_url($pbibo_url);                   // parse url
+  $pbibo_url_parse_host = $pbibo_url_parse["host"];           // get 'host' of the url
+  $pbibo_url_parse_path = $pbibo_url_parse["path"];           // get 'path' of the url
+
+  //$site_url = get_site_url();                                 // get current site URL
+  $site_url = esc_url(get_site_url());
+  
+  $site_url_parse = parse_url($site_url);                     // parse this URL
+  $site_url_parse_host = $site_url_parse["host"];             // get 'host' of the url
+  
+  // compare URLs of pb_is_based_on meta_key AND site URL
+  if ($site_url_parse_host == $pbibo_url_parse_host){
+    $url_to_print = $pbibo_url_parse_path;                    // If they are the same, print only URL path
+  } else {
+    $url_to_print = $pbibo_url;                               // If not, print full URL
+  }
+
+  // generate HTML do be printed inside the metabox
+  $html = '';
+  $html .= '<b>Current URL:</b>';
+  $html .= '<p style="word-wrap:break-word;">'. $url_to_print .'</p><hr>' ;
+  $html .= '<b>Insert new URL:</b>';
+  $html .= '<input name="pb_is_based_on" id="efp_pbibo_metabox" type="url"  placeholder="http://example.com" size="25" />';
+  //echo $html;
+  _e($html);
 }
 
 /**
