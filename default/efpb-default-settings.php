c72845281bded1aebafa273feb63a9aa9c3695e1
CharalamposTheodorou <31764602+CharalamposTheodorou@users.noreply.github.com>
2020-03-04 15:06:10 +0100

    Default Directory Changes
    -  new action hook (deprecated hook ' wpmu_new_blog' replaced with 'wp_insert_site'

--- default/efpb-default-settings.php
+++ default/efpb-default-settings.php
@@ -20,9 +20,14 @@
  */
 
  function efpb_set_default_tagline($blog_id){
- 	switch_to_blog($blog_id);
- 	update_option( 'blogdescription', 'by Books for Languages' );
+	
+	switch_to_blog($blog_id);
+	//modification to sanitize the value before updating to the option.
+	//this may not be needed but it's good practise to use for later. Sanitize everything before updating to the database.
+	update_option( 'blogdescription', sanitize_option('blogdescription','by Books for Languages'));
+
  	restore_current_blog();
  }
- add_action('wpmu_new_blog', 'efpb_set_default_tagline' );
+ add_action('wp_insert_site','efpb_set_default_tagline');
+ //add_action('wpmu_new_blog', 'efpb_set_default_tagline' );
 //possible fix to a deprecated hook -> insert_new_site
