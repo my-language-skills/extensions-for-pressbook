71bf97194686534c4c5213c023211b489703ef98
Antonio D <colomett@gmail.com>
2020-03-08 02:35:46 +0100

    Create reading_time.php

---
+++ reading_time.php
@@ -0,0 +1,15 @@
+  //estimated reading time
+  function reading_time() {
+  $content = get_post_field( 'post_content', $post->ID );
+  $word_count = str_word_count( strip_tags( $content ) );
+  $readingtime = ceil($word_count / 250);
+
+  if ($readingtime == 1) {
+  $timer = " minute";
+  } else {
+  $timer = " minutes";
+  }
+  $totalreadingtime = $readingtime . $timer;
+
+  return $totalreadingtime;
+  }
