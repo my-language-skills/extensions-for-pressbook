5258f9cc4bac366f58d9d1fe3b9972565fda42d6
Colomet <colomet@hotmail.com>
2020-03-30 11:38:44 +0200

    New shortcode

---
+++ shortcodes/efpb-shortcodes.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Extensions for PressBooks / Shortcodes
+ *
+ * This file create new shortcodes.
+ *
+ * @link              URL
+ * @since             1.2.8
+ * @package           extensions-for-pressbooks
+ *
+ **/
+
+
+ /**
+  * Shortcode list of all sites of the multisite for a specific user.
+  * @link https://www.rupokify.com/tech/wordpress/list-blogs-subsites-wordpress-multisite-user/
+  *
+  */
+
+function show_blog_list() {
+
+    $user_id = get_current_user_id();
+    if ($user_id == 0) {
+        echo 'You are not logged in.';
+    } else {
+        echo '<h2>Here is your Book list</h2>';
+        $user_blogs = get_blogs_of_user( $user_id );
+        foreach ($user_blogs AS $user_blog) {
+            echo '<li><a href="'.$user_blog->siteurl.'">'.$user_blog->blogname.'</a></li>';
+        }
+        echo '</ul>';
+    }
+}
+add_shortcode( 'bloglist', 'show_blog_list' );
