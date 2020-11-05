b786ca05a2e4a8200103ffc04a431188008b6c7e
Antonio D <colomett@gmail.com>
2020-11-05 20:09:27 +0100

    form and use metafields

--- h5p/efpb-h5p.php
+++ h5p/efpb-h5p.php
@@ -26,9 +26,15 @@ function x_init_custom_fields() {
 
 // adds a new group to the test post type
 x_add_metadata_group( 'efp_exercises_metabox', 'chapter', array(
-  'label' => 'Exercises'
+  'label' => 'h5p Exercises'
 ) );
 
+	/**
+  * Create the h5p metafield
+  *
+  * @since    1.2.9
+  */
+	
 // adds a number field in the first group (with no min/max)
 // h5p id
 x_add_metadata_field( 'efp_h5p_id_field', 'chapter', array(
@@ -36,6 +42,27 @@ x_add_metadata_field( 'efp_h5p_id_field', 'chapter', array(
     'description' => 'This field It\'s the id of the h5p exercise.', // description for the field
     'field_type' => 'number',
     'required_cap' => 'remove_users',
-    'label' => 'h5p Exercise ID',
+    'label' => 'Exercise ID',
   ) );
+	
+/**
+  * Create the USE and FORM metafields
+  *
+  * @since    1.2.10
+  */
+  x_add_metadata_field( 'efp_h5p_form_id_field', 'chapter', array(
+      'group' => 'efp_exercises_metabox',
+      'description' => 'This field It\'s the id of the h5p Form exercise.', // description for the field
+      'field_type' => 'number',
+      'required_cap' => 'remove_users',
+      'label' => 'FORM Exercise ID',
+    ) );
+
+    x_add_metadata_field( 'efp_h5p_use_id_field', 'chapter', array(
+        'group' => 'efp_exercises_metabox',
+        'description' => 'This field It\'s the id of the h5p Use exercise.', // description for the field
+        'field_type' => 'number',
+        'required_cap' => 'remove_users',
+        'label' => 'USE Exercise ID',
+      ) );
 }
