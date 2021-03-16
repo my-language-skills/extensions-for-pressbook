a92b7997affffdd6fc74380ce145f892f180d129
Colomet <colomet@hotmail.com>
2021-03-16 20:51:51 +0100

    Disable extra h5p fields

--- h5p/efpb-h5p.php
+++ h5p/efpb-h5p.php
@@ -34,7 +34,7 @@ x_add_metadata_group( 'efp_exercises_metabox', 'chapter', array(
   *
   * @since    1.2.9
   */
-	
+
 // adds a number field in the first group (with no min/max)
 // h5p id
 x_add_metadata_field( 'efp_h5p_id_field', 'chapter', array(
@@ -44,25 +44,25 @@ x_add_metadata_field( 'efp_h5p_id_field', 'chapter', array(
     'required_cap' => 'remove_users',
     'label' => 'Exercise ID',
   ) );
-	
+
 /**
   * Create the USE and FORM metafields
   *
   * @since    1.2.10
   */
-  x_add_metadata_field( 'efp_h5p_form_id_field', 'chapter', array(
-      'group' => 'efp_exercises_metabox',
-      'description' => 'This field It\'s the id of the h5p Form exercise.', // description for the field
-      'field_type' => 'number',
-      'required_cap' => 'remove_users',
-      'label' => 'FORM Exercise ID',
-    ) );
-
-    x_add_metadata_field( 'efp_h5p_use_id_field', 'chapter', array(
-        'group' => 'efp_exercises_metabox',
-        'description' => 'This field It\'s the id of the h5p Use exercise.', // description for the field
-        'field_type' => 'number',
-        'required_cap' => 'remove_users',
-        'label' => 'USE Exercise ID',
-      ) );
+  // x_add_metadata_field( 'efp_h5p_form_id_field', 'chapter', array(
+  //     'group' => 'efp_exercises_metabox',
+  //     'description' => 'This field It\'s the id of the h5p Form exercise.', // description for the field
+  //     'field_type' => 'number',
+  //     'required_cap' => 'remove_users',
+  //     'label' => 'FORM Exercise ID',
+  //   ) );
+  //
+  //   x_add_metadata_field( 'efp_h5p_use_id_field', 'chapter', array(
+  //       'group' => 'efp_exercises_metabox',
+  //       'description' => 'This field It\'s the id of the h5p Use exercise.', // description for the field
+  //       'field_type' => 'number',
+  //       'required_cap' => 'remove_users',
+  //       'label' => 'USE Exercise ID',
+  //     ) );
 }
